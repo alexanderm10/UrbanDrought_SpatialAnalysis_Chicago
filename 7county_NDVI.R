844ed6f0a9493e2fc01f9abe5c8137b623ba55af
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2023-04-18 12:39:33 -0500

    Update 7county_NDVI.R

--- 7county_NDVI.R
+++ 7county_NDVI.R
@@ -12,7 +12,7 @@ path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive
 #reading the csv
 dat.7c <- read.csv(file.path(path.google, "NDVI_7county_bookends.csv"))
 
-head(dat.7c)
+View(dat.7c)
 
 
 
@@ -24,29 +24,9 @@ head(dat.7c)
 #attemtption to generte a graph
 #png(file.path(path.figs,"Seven County NDVI"), height=4, width=6, units="in", res=320)
 ggplot(data=dat.7c) +
- facet_wrap(~NAME )+
-  geom_smooth(aes(x=month, y=mean, color=as.factor(year)))+
-  scale_color_manual(name="year", values=c("2004"="NA", "2005"="red", "2006"="NA", "2011"="NA", "2012"="#F0E442", "2013"="NA", "2020"="NA", "2021"= "violet","2022"="NA")) +
+ facet_grid(~NAME )+
+  geom_boxplot(aes(x=month, y=mean, fill=as.factor(year)))+
   labs(title="Seven County NDVI", y="NDVI", x="month", color="year")+
   theme_classic()
 dev.off()
-
-
-ggplot(data=dat.7c) +
-  facet_wrap(~NAME )+
-  geom_line(aes(x=month, y=mean, color=as.factor(year)))+
-  #scale_color_manual(name="year", values=c("2004"="NA", "2005"="red", "2006"="NA", "2011"="NA", "2012"="#F0E442", "2013"="NA", "2020"="NA", "2021"= "violet","2022"="NA")) +
-  labs(title="Seven County NDVI", y="NDVI", x="month", color="year")+
-  theme_classic()
-dev.off()
-
-ggplot(data=dat.7c) +
-  facet_wrap(~NAME )+
-  geom_point(alpha= 0.5,aes(x=month, y=mean, color=as.factor(year)))+
-  #scale_color_manual(name="year", values=c("2004"="NA", "2005"="red", "2006"="NA", "2011"="NA", "2012"="#F0E442", "2013"="NA", "2020"="NA", "2021"= "violet","2022"="NA")) +
-  labs(title="Seven County NDVI", y="NDVI", x="month", color="year")+
-  theme_classic()
-dev.off()
-
-
 
