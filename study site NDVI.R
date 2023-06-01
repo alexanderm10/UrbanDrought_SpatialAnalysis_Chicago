cdbf866b9b9dbde9f94c6973962a30cefde24cc0
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2023-06-01 13:17:51 -0500

    Script of NDVI at the 7 locations for the urban ecological drought project
    Added MODIS observations for the 7 sites, simple graphing for trends

--- study site NDVI.R
+++ study site NDVI.R
@@ -1,16 +1,99 @@
 library(ggplot2)
-path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry/"
-lmmu<- read.csv(file.path(path.google, "NDVI_Lombard_Municipal.csv"))
-head(sitendvi)
+##setting up my path
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
 
-ggplot(data=sitendvi)+
-  facet_wrap(~ site) +
-  aes(x=month, y=meanNDVI, group=year) +
+
+##reading in the csv's selecting the columns I want
+#Lombard municipal
+lmmu<- read.csv(file.path(path.google, "NDVI_Lombard_Municipal.csv"))
+head(lmmu)
+dat.lm <- lmmu[, c("system.index", "meanNDVI")]
+dat.lm$name<-"Lombard Municipal"
+head(dat.lm)
+
+#Thornhill 
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+thill<- read.csv(file.path(path.google, "NDVI_Thornhill.csv"))
+head(thill)
+dat.th <- thill[, c("system.index", "meanNDVI")]
+dat.th$name <- "Thornhill Lot "
+head(dat.th)
+
+#research lot
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+rslt<- read.csv(file.path(path.google, "NDVI_Research_Lot.csv"))
+head(rslt)
+dat.rl <- rslt[, c("system.index", "meanNDVI")]
+dat.rl$name <- "Research Lot"
+head(dat.rl)
+
+#Big Grass
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+bg<- read.csv(file.path(path.google, "NDVI_Big_Grass.csv"))
+head(bg)
+dat.bg <- bg[, c("system.index", "meanNDVI")]
+dat.bg$name <- "Big Grass Lot"
+head(dat.bg)
+
+#Morotn Library 
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+lb<- read.csv(file.path(path.google, "NDVI_Library.csv"))
+head(lb)
+dat.lb <- lb[, c("system.index", "meanNDVI")]
+dat.lb$name <- "Morton Library Lot "
+head(dat.lb)
+
+#Lombard Main Street
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+ms<- read.csv(file.path(path.google, "NDVI_Main_Street.csv"))
+head(ms)
+dat.ms <- ms[, c("system.index", "meanNDVI")]
+dat.ms$name <- "Lombard Main Street"
+head(dat.ms)
+
+#UIC Lot 5
+path.google <- "~/Library/CloudStorage/GoogleDrive-breidy@mortonarb.org/My Drive/eesundry"
+l5<- read.csv(file.path(path.google, "NDVI_UIC Lot_5.csv"))
+head(l5)
+dat.l5 <- l5[, c("system.index", "meanNDVI")]
+dat.l5$name <- "UIC Lot 5"
+head(dat.l5)
+
+#and in the darkness bind them 
+dat.all <- rbind(dat.bg,dat.l5,dat.lb,dat.rl, dat.ms,dat.lm,dat.th)
+
+
+
+# Rename the "meanNDVI" column to "NDVI"
+colnames(dat.all)[colnames(dat.all) == "meanNDVI"] <- "NDVI"
+
+# Change the "system.index" column to "date" and convert it to the appropriate format
+dat.all$date <- as.Date(dat.all$system.index, format = "%Y_%m_%d")
+
+dat.all$year <- lubridate::year(as.Date(dat.all$date))
+dat.all$yday <- lubridate::yday(as.Date(dat.all$date))
+head(dat.all)
+
+#plotting
+ggplot(data=dat.all)+
+ facet_wrap(name~.) +
+  aes(x=yday, y=NDVI, group=year) +
   geom_line()+
-  geom_line(data=sitendvi[sitendvi$year==2012, ], aes(color="2012"),size=1.0) +
-  geom_line(data=sitendvi[sitendvi$year==2005, ], aes(color="2005"),size=1.0) +
-  geom_line(data=sitendvi[sitendvi$year==2021, ], aes(color="2021"),size=1.0) +
+  geom_line(data=dat.all[dat.all$year==2012, ], aes(color="2012"),size=1.0) +
+  geom_line(data=dat.all[dat.all$year==2005, ], aes(color="2005"),size=1.0) +
+  geom_line(data=dat.all[dat.all$year==2021, ], aes(color="2021"),size=1.0) +
   scale_color_manual(values=c("2012"='goldenrod', '2005'="red3", '2021'="lightblue")) +
-  labs(title="Urban Ecological drought study site NDVI", x="Month") 
+  labs(title="Urban Ecological drought study site NDVI", x="Yday") 
 #dev.off()
 
+##messing around with spacing the plot
+ggplot(data = dat.all) +
+  facet_wrap(name ~ ., ncol = 2, as.table = TRUE) +
+  aes(x = yday, y = NDVI, group = year) +
+  geom_line() +
+  geom_line(data = dat.all[dat.all$year == 2012, ], aes(color = "2012"), size = 1.5) +
+  geom_line(data = dat.all[dat.all$year == 2005, ], aes(color = "2005"), size = 1.5) +
+  geom_line(data = dat.all[dat.all$year == 2021, ], aes(color = "2021"), size = 1.5) +
+  scale_color_manual(values = c("2012" = "goldenrod", "2005" = "red3", "2021" = "lightblue")) +
+  labs(title = "Urban Ecological Drought Study Site NDVI", x = "Yday") +
+  theme(strip.text = element_text(margin = margin(0, 0, 10, 0)))
