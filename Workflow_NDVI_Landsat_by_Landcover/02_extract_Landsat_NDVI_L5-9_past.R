4542bb30f7d5d556162839e26927eb15078e9ffc
Christy Rollinson <crollinson@mortonarb.org>
2023-07-06 13:17:24 -0500

    add NLCD & start of NDVI extraction

--- Workflow_NDVI_Landsat_by_Landcover/02_extract_Landsat_NDVI_L5-9_past.R
+++ Workflow_NDVI_Landsat_by_Landcover/02_extract_Landsat_NDVI_L5-9_past.R
@@ -9,3 +9,143 @@
 # 4. Create landcover-specific collections using the existing nlcd-based masks (see script 01) 
 # 5. Reduce to the mean value for each image date
 # 6. Save a file for each Landsat product (e.g. Landsat 7, Landsat 8) with the time series for each landcover class -- 1 value per landcover class per date
+
+library(rgee); library(raster); library(terra)
+ee_check() # For some reason, it's important to run this before initializing right now
+rgee::ee_Initialize(user = 'crollinson@mortonarb.org', drive=T)
+path.google.CR <- "~/Google Drive/My Drive/UrbanEcoDrought/"
+path.google.share <- "~/Google Drive/Shared drives/Urban Ecological Drought/"
+# GoogleFolderSave <- "UHI_Analysis_Output_Final_v2"
+assetHome <- ee_get_assethome()
+
+##################### 
+# 0. Set up helper functions ----
+##################### 
+addTime <- function(image){
+  return(image$addBands(image$metadata('system:time_start')$divide(1000 * 60 * 60 * 24 * 365)))
+}
+
+setYear <- function(img){
+  return(img$set("year", img$date()$get("year")))
+}
+
+addYear = function(img) {
+  d= ee$Date(ee$Number(img$get('system:time_start')));
+  y= ee$Number(d$get('year'));
+  return img$set('year', y);
+}
+
+bitwiseExtract <- function(input, fromBit, toBit) {
+  maskSize <- ee$Number(1)$add(toBit)$subtract(fromBit)
+  mask <- ee$Number(1)$leftShift(maskSize)$subtract(1)
+  return(input$rightShift(fromBit)$bitwiseAnd(mask))
+}
+
+addNDVI <- function(img){
+  return( img$addBands(img$normalizedDifference(c('nir','red'))$rename('NDVI')));
+}
+
+
+applyLandsatBitMask = function(img){
+  qaPix <- img$select('QA_PIXEL');
+  qaRad <- img$select('QA_RADSAT');
+  terrMask <- qaRad$bitwiseAnd(11)$eq(0); ## get rid of any terrain occlusion
+  # satMask <- qaRad$bitwiseAnd(3 << 4)$eq(0); ## get rid of any saturated bands we use to calculate NDVI
+  satMask <- bitwiseExtract(qaRad, 3, 4)$eq(0) ## get rid of any saturated bands we use to calculate NDVI 
+  # clearMask <- qaPix$bitwiseAnd(1<<7)$eq(0)
+  clearMask <- bitwiseExtract(qaPix, 1, 7)$eq(0) 
+  cloudConf = bitwiseExtract(qaPix, 8, 9)$lte(1) ## we can only go with low confidence; doing finer leads to NOTHING making the cut
+  shadowConf <- bitwiseExtract(qaPix, 10, 11)$lte(1) ## we can only go with low confidence; doing finer leads to NOTHING making the cut
+  snowConf <- bitwiseExtract(qaPix, 12, 13)$lte(1) ## we can only go with low confidence; doing finer leads to NOTHING making the cut
+  
+  
+  img <- img$updateMask(clearMask$And(cloudConf)$And(shadowConf)$And(snowConf)$And(terrMask)$And(satMask));
+  
+  return(img)
+  
+}
+
+# Function for combining images with the same date
+# 2nd response from here: https:#gis.stackexchange.com/questions/280156/mosaicking-image-collection-by-date-day-in-google-earth-engine 
+mosaicByDate <- function(imcol, dayWindow){
+  # imcol: An image collection
+  # returns: An image collection
+  imlist = imcol$toList(imcol$size())
+   
+  # Note: needed to specify the ee_utils_pyfunc since it's not an image collection
+  unique_dates <- imlist$map(ee_utils_pyfunc(function(img){
+    return(ee$Image(img)$date()$format("YYYY-MM-dd"))
+  }))$distinct()
+  
+  # Same as above: what we're mappign through is a List, so need to call python
+  mosaic_imlist = unique_dates$map(ee_utils_pyfunc(function(d){
+    d = ee$Date(d)
+    dy= d$get('day');    
+    m= d$get('month');
+    y= d$get('year');
+    
+    im = imcol$filterDate(d$advance(-dayWindow, "day"), d$advance(dayWindow, "day"))$reduce(ee$Reducer$median()) # shoudl influence the window for image aggregation
+    
+    return(im$set("system:time_start", d$millis(), 
+      "system:id", d$format("YYYY-MM-dd"),
+      'date', d, 'day', dy, 'month', m, 'year', y))
+    }))
+  
+  # testOUT <- ee$ImageCollection(mosaic_imlist)
+  # ee_print(testOUT)
+  return (ee$ImageCollection(mosaic_imlist))
+}
+##################### 
+
+
+##################### 
+# Color Palette etc. ----
+##################### 
+# Setting the center point for the Arb because I think we'll have more variation
+Map$setCenter(-88.04526, 41.81513, 11);
+
+ndviVis = list(
+  min= 0.0,
+  max= 1,
+  palette= c(
+    '#FFFFFF', '#CE7E45', '#DF923D', '#F1B555', '#FCD163', '#99B718', '#74A901',
+    '#66A000', '#529400', '#3E8601', '#207401', '#056201', '#004C00', '#023B01',
+    '#012E01', '#011D01', '#011301'
+    )
+  )
+##################### 
+
+
+##################### 
+# Extract Landsat 8
+##################### 
+# "LANDSAT/LC08/C02/T1_RT"
+# Load MODIS NDVI data; attach month & year
+# https:#developers.google.com/earth-engine/datasets/catalog/LANDSAT_LC08_C02_T1_L2
+landsat8 <- ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")$filterBounds(Chicago)$map(function(image){
+  return(image$clip(Chicago))
+})$map(function(img){
+  d= ee$Date(img$get('system:time_start'));
+  dy= d$get('day');    
+  m= d$get('month');
+  y= d$get('year');
+  
+  # # Add masks 
+  img <- applyLandsatBitMask(img)
+  
+  # #scale correction; doing here & separating form NDVI so it gets saved on the image
+  lAdj = img$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'))$multiply(0.0000275)$add(-0.2);
+  lst_k = img$select('ST_B10')$multiply(0.00341802)$add(149);
+
+  # img3 = img2$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y)
+  return(img$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y))
+})$select(c('SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7', 'ST_B10'),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K'))$map(addNDVI)
+
+# ee_print(landsat8, "landsat8")
+
+l8Mosaic = mosaicByDate(landsat8, 7)$sort("date")
+# ee_print(l8Mosaic, "landsat8-Mosaic")
+# Map.addLayer(l8Mosaic.first().select('NDVI_median'), ndviVis, "NDVI - First")
+##################### 
+
+
