b4e6ed19732b338ed81b0cb0c57263a167f2304c
Christy Rollinson <crollinson@gmail.com>
2023-07-25 16:25:24 -0500

    Pull additional Landsat missions!!
    Woohooo!!!! It's running!!

--- Workflow_NDVI_Landsat_by_Landcover/02_extract_Landsat_NDVI_L5-9_past.R
+++ Workflow_NDVI_Landsat_by_Landcover/02_extract_Landsat_NDVI_L5-9_past.R
@@ -51,6 +51,8 @@ ee_print(Chicago)
 chiBounds <- Chicago$geometry()$bounds()
 chiBBox <- ee$Geometry$BBox(-88.70738, 41.20155, -87.52453, 42.49575)
 
+# Landcover names and mask ----
+lcnames <- c("forest", "crop", "grassland", "urban-high", "urban-medium", "urban-low", "urban-open")
 
 forMask <- ee$Image('users/crollinson/NLCD-Chicago_2000-2023_Forest')
 # ee_print(forMask)
@@ -78,7 +80,7 @@ urbHMask <- ee$Image('users/crollinson/NLCD-Chicago_2000-2023_Urban-High')
 ##################### 
 # "LANDSAT/LC08/C02/T1_RT"
 # Load MODIS NDVI data; attach month & year
-# https:#developers.google.com/earth-engine/datasets/catalog/LANDSAT_LC08_C02_T1_L2
+# https://developers.google.com/earth-engine/datasets/catalog/LANDSAT_LC08_C02_T1_L2
 landsat8 <- ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")$filterBounds(Chicago)$map(function(image){
   return(image$clip(Chicago))
 })$map(function(img){
@@ -104,15 +106,133 @@ landsat8 <- ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")$filterBounds(Chicago)$m
 l8Mosaic = mosaicByDate(landsat8, 7)$select(c('blue_median', 'green_median', 'red_median', 'nir_median', 'swir1_median', 'swir2_median', 'LST_K_median', "NDVI_median"),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K', "NDVI"))$sort("date")
 # ee_print(l8Mosaic, "landsat8-Mosaic")
 # Map$addLayer(l8Mosaic$first()$select('NDVI'), ndviVis, "NDVI - First")
-##################### 
 
-##################### 
-# Mask NDVI by Landcover & condense to regional means ----
-##################### 
-lcnames <- c("forest", "crop", "grassland", "urban-high", "urban-medium", "urban-low", "urban-open")
+# Mask NDVI by Landcover & condense to regional means
 for(LCTYPE in lcnames){
   # print(LCTYPE)
   extractByLC(imcol=l8Mosaic, landcover=LCTYPE, outfolder="UrbanEcoDrought_TEST", fileNamePrefix=paste0("Landsat8_", LCTYPE))
 }
 
+##################### 
 
+##################### 
+# Read in & Format Landsat 9 ----
+##################### 
+# "LANDSAT/LC09/C02/T1_L2"
+# Load MODIS NDVI data; attach month & year
+# https://developers.google.com/earth-engine/datasets/catalog/LANDSAT_LC09_C02_T1_L2
+landsat9 <- ee$ImageCollection("LANDSAT/LC09/C02/T1_L2")$filterBounds(Chicago)$map(function(image){
+  return(image$clip(Chicago))
+})$map(function(img){
+  d= ee$Date(img$get('system:time_start'));
+  dy= d$get('day');    
+  m= d$get('month');
+  y= d$get('year');
+  
+  # # Add masks 
+  img <- applyLandsatBitMask(img)
+  
+  # #scale correction; doing here & separating form NDVI so it gets saved on the image
+  lAdj = img$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'))$multiply(0.0000275)$add(-0.2);
+  lst_k = img$select('ST_B10')$multiply(0.00341802)$add(149);
+  
+  # img3 = img2$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y)
+  return(img$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y))
+})$select(c('SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7', 'ST_B10'),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K'))$map(addNDVI)
+# Map$addLayer(landsat9$first()$select('NDVI'), ndviVis, "NDVI - First")
+# ee_print(landsat9)
+# Map$addLayer(landsat9$first()$select('NDVI'))
+
+l9Mosaic = mosaicByDate(landsat9, 7)$select(c('blue_median', 'green_median', 'red_median', 'nir_median', 'swir1_median', 'swir2_median', 'LST_K_median', "NDVI_median"),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K', "NDVI"))$sort("date")
+# ee_print(l9Mosaic, "landsat9-Mosaic")
+# Map$addLayer(l9Mosaic$first()$select('NDVI'), ndviVis, "NDVI - First")
+
+# Mask NDVI by Landcover & condense to regional means
+for(LCTYPE in lcnames){
+  # print(LCTYPE)
+  extractByLC(imcol=l9Mosaic, landcover=LCTYPE, outfolder="UrbanEcoDrought_TEST", fileNamePrefix=paste0("Landsat9_", LCTYPE))
+}
+
+##################### 
+
+##################### 
+# Read in & Format Landsat 7 ----
+##################### 
+# ""LANDSAT/LE07/C02/T1_L2""
+# Load MODIS NDVI data; attach month & year
+# https://developers.google.com/earth-engine/datasets/catalog/LANDSAT_LE07_C02_T1_L2
+landsat7 <- ee$ImageCollection("LANDSAT/LE07/C02/T1_L2")$filterBounds(Chicago)$filterDate("2001-01-01", "2022-12-31")$map(function(image){
+  return(image$clip(Chicago))
+})$map(function(img){
+  d= ee$Date(img$get('system:time_start'));
+  dy= d$get('day');    
+  m= d$get('month');
+  y= d$get('year');
+  
+  # # Add masks 
+  img <- applyLandsatBitMask(img)
+  
+  # #scale correction; doing here & separating form NDVI so it gets saved on the image
+  lAdj = img$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7'))$multiply(0.0000275)$add(-0.2);
+  lst_k = img$select('ST_B6')$multiply(0.00341802)$add(149);
+  
+  # img3 = img2$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y)
+  return(img$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y))
+})$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7', 'ST_B6'),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K'))$map(addNDVI)
+# Map$addLayer(landsat7$first()$select('NDVI'), ndviVis, "NDVI - First")
+# ee_print(landsat7)
+# Map$addLayer(landsat7$first()$select('NDVI'))
+
+l7Mosaic = mosaicByDate(landsat7, 7)$select(c('blue_median', 'green_median', 'red_median', 'nir_median', 'swir1_median', 'swir2_median', 'LST_K_median', "NDVI_median"),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K', "NDVI"))$sort("date")
+# ee_print(l7Mosaic, "landsat7-Mosaic")
+# Map$addLayer(l7Mosaic$first()$select('NDVI'), ndviVis, "NDVI - First")
+
+# Mask NDVI by Landcover & condense to regional means
+for(LCTYPE in lcnames){
+  # print(LCTYPE)
+  extractByLC(imcol=l7Mosaic, landcover=LCTYPE, outfolder="UrbanEcoDrought_TEST", fileNamePrefix=paste0("Landsat7_", LCTYPE))
+}
+
+##################### 
+
+
+##################### 
+# Read in & Format Landsat 5 ----
+##################### 
+# "LANDSAT_LT05_C02_T1_L2"
+# Load MODIS NDVI data; attach month & year
+# https://developers.google.com/earth-engine/datasets/catalog/LANDSAT_LT05_C02_T1_L2
+landsat5 <- ee$ImageCollection("LANDSAT/LT05/C02/T1_L2")$filterBounds(Chicago)$filterDate("2001-01-01", "2022-12-31")$map(function(image){
+  return(image$clip(Chicago))
+})$map(function(img){
+  d= ee$Date(img$get('system:time_start'));
+  dy= d$get('day');    
+  m= d$get('month');
+  y= d$get('year');
+  
+  # # Add masks 
+  img <- applyLandsatBitMask(img)
+  
+  # #scale correction; doing here & separating form NDVI so it gets saved on the image
+  lAdj = img$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7'))$multiply(0.0000275)$add(-0.2);
+  lst_k = img$select('ST_B6')$multiply(0.00341802)$add(149);
+  
+  # img3 = img2$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y)
+  return(img$addBands(srcImg=lAdj, overwrite=T)$addBands(srcImg=lst_k, overwrite=T)$set('date',d, 'day',dy, 'month',m, 'year',y))
+})$select(c('SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7', 'ST_B6'),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K'))$map(addNDVI)
+# Map$addLayer(landsat5$first()$select('NDVI'), ndviVis, "NDVI - First")
+# ee_print(landsat5)
+# Map$addLayer(landsat5$first()$select('NDVI'))
+
+l5Mosaic = mosaicByDate(landsat5, 7)$select(c('blue_median', 'green_median', 'red_median', 'nir_median', 'swir1_median', 'swir2_median', 'LST_K_median', "NDVI_median"),c('blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'LST_K', "NDVI"))$sort("date")
+# ee_print(l5Mosaic, "landsat5-Mosaic")
+# Map$addLayer(l5Mosaic$first()$select('NDVI'), ndviVis, "NDVI - First")
+
+# Mask NDVI by Landcover & condense to regional means
+for(LCTYPE in lcnames){
+  # print(LCTYPE)
+  extractByLC(imcol=l5Mosaic, landcover=LCTYPE, outfolder="UrbanEcoDrought_TEST", fileNamePrefix=paste0("Landsat5_", LCTYPE))
+}
+
+
+##################### 
--- Workflow_NDVI_Landsat_by_Landcover/12_graph_Landsat_NDVI.R
+++ Workflow_NDVI_Landsat_by_Landcover/12_graph_Landsat_NDVI.R
@@ -12,48 +12,49 @@ library(ggplot2)
 path.google <- ("~/Google Drive/My Drive/UrbanEcoDrought_TEST")
 
 # Clunky code, but should pull the latest file
-LCforest <- read.csv(file.path(path.google, dir(path.google, "Landsat8_forest")[length(dir(path.google, "Landsat8_forest"))]))
-LCgrass <- read.csv(file.path(path.google, dir(path.google, "Landsat8_grassland")[length(dir(path.google, "Landsat8_grassland"))]))
-LCcrop <- read.csv(file.path(path.google, dir(path.google, "Landsat8_crop")[length(dir(path.google, "Landsat8_crop"))]))
-LCurbO <- read.csv(file.path(path.google, dir(path.google, "Landsat8_urban-open")[length(dir(path.google, "Landsat8_urban-open"))]))
-LCurbL <- read.csv(file.path(path.google, dir(path.google, "Landsat8_urban-low")[length(dir(path.google, "Landsat8_urban-low"))]))
-LCurbM <- read.csv(file.path(path.google, dir(path.google, "Landsat8_urban-med")[length(dir(path.google, "Landsat8_urban-med"))]))
-LCurbH <- read.csv(file.path(path.google, dir(path.google, "Landsat8_urban-high")[length(dir(path.google, "Landsat8_urban-high"))]))
-summary(forest)
+lcnames <- c("forest", "crop", "grassland", "urban-high", "urban-medium", "urban-low", "urban-open")
 
-LCforest$type = "forest"
-LCgrass$type = "grassland"
-LCcrop$type = "crop"
-LCurbO$type = "urban-open"
-LCurbL$type = "urban-low"
-LCurbM$type = "urban-med"
-LCurbH$type = "urban-high"
+ndviAll <- data.frame()
+for(LCTYPE in lcnames){
+  landsat8 <- read.csv(file.path(path.google, dir(path.google, paste0("Landsat8_", LCTYPE))[length(dir(path.google, paste0("Landsat8_", LCTYPE)))]))
+  landsat9 <- read.csv(file.path(path.google, dir(path.google, paste0("Landsat9_", LCTYPE))[length(dir(path.google, paste0("Landsat9_", LCTYPE)))]))
+  landsat7 <- read.csv(file.path(path.google, dir(path.google, paste0("Landsat7_", LCTYPE))[length(dir(path.google, paste0("Landsat7_", LCTYPE)))]))
+  landsat5 <- read.csv(file.path(path.google, dir(path.google, paste0("Landsat5_", LCTYPE))[length(dir(path.google, paste0("Landsat5_", LCTYPE)))]))
+  
+  landsat8$mission <- "landsat 8"
+  landsat9$mission <- "landsat 9"
+  landsat7$mission <- "landsat 7"
+  landsat5$mission <- "landsat 5"
+  
+  landsatAll <- rbind(landsat8, landsat9, landsat7, landsat5)
+  # landsatAll <- rbind(landsat8, landsat9)
+  landsatAll$type <- LCTYPE
+  
+  ndviAll <- rbind(ndviAll, landsatAll)
+}
+summary(ndviAll)
 
-head(forest)
-
-
-ndvi <- rbind(forest, grass, crop, urbO, urbL, urbM, urbH) 
-ndvi$date <- as.Date(ndvi$date)
-ndvi$year <- lubridate::year(ndvi$date)
-ndvi$yday <- lubridate::yday(ndvi$date)
-ndvi$type <- factor(ndvi$type, levels=rev(c("forest", "grassland", "crop", "urban-open", "urban-low", "urban-med", "urban-high")))
-head(ndvi)
-summary(ndvi)
+ndviAll$date <- as.Date(ndviAll$date)
+ndviAll$year <- lubridate::year(ndviAll$date)
+ndviAll$yday <- lubridate::yday(ndviAll$date)
+ndviAll$type <- factor(ndviAll$type, levels=rev(c("forest", "grassland", "crop", "urban-open", "urban-low", "urban-med", "urban-high")))
+head(ndviAll)
+summary(ndviAll)
 
 png("~/Google Drive/Shared drives/Urban Ecological Drought/Neighborhood remote sensing analysis/NDVI_Landsat_NLCD_latest.png", height=8, width=11, units="in", res=320)
-ggplot(data=ndvi[,], aes(x=yday, y=NDVI)) +
-  ggtitle(paste0("Landsat 8 NDVI, last image: ", max(ndvi$date))) +
+ggplot(data=ndviAll[,], aes(x=yday, y=NDVI)) +
+  ggtitle(paste0("Landsat 5,7,8,9 NDVI, last image: ", max(ndviAll$date))) +
   facet_wrap(~type) +
   # stat_smooth(color="black", fill=NA, size=0.5) +
   geom_line(aes(group=year), color="gray30", size=0.1)+
-  # geom_line(data=ndvi[ndvi$year==2005, ], aes(color="2005"), size=0.25) +
-  # geom_line(data=ndvi[ndvi$year==2012, ], aes(color="2012"), size=0.25) +
-  geom_line(data=ndvi[ndvi$year==2023, ], aes(color="2023"), size=0.5) +
-  stat_smooth(data=ndvi[!ndvi$year %in% c(2005, 2012, 2023), ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
-  # stat_smooth(data=ndvi[ndvi$yday<=max(ndvi$yday[ndvi$year==2023]) & ndvi$year!=2023, ], aes(color="historical", fill="historical"), size=1.5, alpha=0.8) +
-  # stat_smooth(data=ndvi[ndvi$year==2005, ], aes(color="2005", fill="2005"), size=1, alpha=0.2, method="gam") +
-  # stat_smooth(data=ndvi[ndvi$year==2012, ], aes(color="2012", fill="2012"), size=1, alpha=0.2, method="gam") +
-  stat_smooth(data=ndvi[ndvi$year==2023, ], aes(color="2023", fill="2023"), size=1.25, alpha=0.5, method="gam") +
+  geom_line(data=ndviAll[ndviAll$year==2005, ], aes(color="2005"), size=0.25) +
+  geom_line(data=ndviAll[ndviAll$year==2012, ], aes(color="2012"), size=0.25) +
+  geom_line(data=ndviAll[ndviAll$year==2023, ], aes(color="2023"), size=0.5) +
+  stat_smooth(data=ndviAll[!ndviAll$year %in% c(2005, 2012, 2023), ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
+  # stat_smooth(data=ndviAll[ndviAll$yday<=max(ndviAll$yday[ndviAll$year==2023]) & ndviAll$year!=2023, ], aes(color="historical", fill="historical"), size=1.5, alpha=0.8) +
+  stat_smooth(data=ndviAll[ndviAll$year==2005, ], aes(color="2005", fill="2005"), size=1, alpha=0.2, method="gam") +
+  stat_smooth(data=ndviAll[ndviAll$year==2012, ], aes(color="2012", fill="2012"), size=1, alpha=0.2, method="gam") +
+  stat_smooth(data=ndviAll[ndviAll$year==2023, ], aes(color="2023", fill="2023"), size=1.25, alpha=0.5, method="gam") +
   scale_color_manual(values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
   scale_fill_manual(values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
   coord_cartesian(ylim=c(0,1)) +
