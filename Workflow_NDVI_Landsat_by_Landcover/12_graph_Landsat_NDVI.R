7a1a5532d451d0b662fdfbb9c253d309328bdc95
Christy Rollinson <crollinson@mortonarb.org>
2024-07-10 18:55:42 -0500

    Add code to copy Landsat files

--- Workflow_NDVI_Landsat_by_Landcover/12_graph_Landsat_NDVI.R
+++ Workflow_NDVI_Landsat_by_Landcover/12_graph_Landsat_NDVI.R
@@ -12,8 +12,8 @@ library(ggplot2)
 path.google <- ("~/Google Drive/My Drive/")
 # NDVIsave <- "UrbanEcoDrought_NDVI_LocalExtract"
 NDVIsave <- "UrbanEcoDrought_NDVI_LocalExtract"
-pathShare <- file.path(path.google, "Shared drives/Urban Ecological Drought/data/UrbanEcoDrought_NDVI_LocalExtract/")
-
+pathShare <- file.path(path.google, "../Shared drives/Urban Ecological Drought/data/UrbanEcoDrought_NDVI_LocalExtract/")
+# pathShare <- "~/Google Drive/Shared drives/Urban Ecological Drought/data/UrbanEcoDrought_NDVI_LocalExtract/"
 
 fNDVI <- dir(file.path(path.google, NDVIsave))
 
@@ -29,10 +29,20 @@ lcnames <- c("forest", "crop", "grassland", "urban-high", "urban-medium", "urban
 
 ndviAll <- data.frame()
 for(LCTYPE in lcnames){
-  landsat8 <- read.csv(file.path(path.google, NDVIsave, dir(file.path(path.google, NDVIsave), paste0("Landsat8_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat8_", LCTYPE)))]))
-  landsat9 <- read.csv(file.path(path.google, NDVIsave, dir(file.path(path.google, NDVIsave), paste0("Landsat9_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat9_", LCTYPE)))]))
-  landsat7 <- read.csv(file.path(path.google, NDVIsave, dir(file.path(path.google, NDVIsave), paste0("Landsat7_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat7_", LCTYPE)))]))
-  landsat5 <- read.csv(file.path(path.google, NDVIsave, dir(file.path(path.google, NDVIsave), paste0("Landsat5_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat5_", LCTYPE)))]))
+  fileL8 <- dir(file.path(path.google, NDVIsave), paste0("Landsat8_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat8_", LCTYPE)))]
+  fileL9 <- dir(file.path(path.google, NDVIsave), paste0("Landsat9_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat9_", LCTYPE)))]
+  fileL7 <- dir(file.path(path.google, NDVIsave), paste0("Landsat7_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat7_", LCTYPE)))]
+  fileL5 <- dir(file.path(path.google, NDVIsave), paste0("Landsat5_", LCTYPE))[length(dir(file.path(path.google, NDVIsave), paste0("Landsat5_", LCTYPE)))]
+  
+  if(!file.exists(file.path(pathShare, fileL8))) file.copy(from=file.path(path.google, NDVIsave, fileL8), to=file.path(pathShare, fileL8))
+  if(!file.exists(file.path(pathShare, fileL9))) file.copy(from=file.path(path.google, NDVIsave, fileL9), to=file.path(pathShare, fileL9))
+  if(!file.exists(file.path(pathShare, fileL7))) file.copy(from=file.path(path.google, NDVIsave, fileL7), to=file.path(pathShare, fileL7))
+  if(!file.exists(file.path(pathShare, fileL5))) file.copy(from=file.path(path.google, NDVIsave, fileL5), to=file.path(pathShare, fileL5))
+  
+  landsat8 <- read.csv(file.path(path.google, NDVIsave, fileL8))
+  landsat9 <- read.csv(file.path(path.google, NDVIsave, fileL9))
+  landsat7 <- read.csv(file.path(path.google, NDVIsave, fileL7))
+  landsat5 <- read.csv(file.path(path.google, NDVIsave, fileL5))
   
   landsat8$mission <- "landsat 8"
   landsat9$mission <- "landsat 9"
@@ -56,6 +66,8 @@ head(ndviAll)
 summary(ndviAll)
 unique(ndviAll$type)
 
+write.csv(ndviAll, file.path(pathShare, "NDVIall_latest.csv"), row.names=F)
+
 png("~/Google Drive/Shared drives/Urban Ecological Drought/Neighborhood remote sensing analysis/NDVI_Landsat_NLCD_latest.png", height=8, width=11, units="in", res=320)
 ggplot(data=ndviAll[,], aes(x=yday, y=NDVI)) +
   ggtitle(paste0("Landsat 5,7,8,9 NDVI, last image: ", max(ndviAll$date))) +
@@ -65,14 +77,16 @@ ggplot(data=ndviAll[,], aes(x=yday, y=NDVI)) +
   geom_line(data=ndviAll[ndviAll$year==2005, ], aes(color="2005"), size=0.25) +
   geom_line(data=ndviAll[ndviAll$year==2012, ], aes(color="2012"), size=0.25) +
   geom_line(data=ndviAll[ndviAll$year==2023, ], aes(color="2023"), size=0.5) +
-  stat_smooth(data=ndviAll[!ndviAll$year %in% c(2005, 2012, 2023), ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
+  geom_line(data=ndviAll[ndviAll$year==2024, ], aes(color="2024"), size=0.5) +
+  stat_smooth(data=ndviAll[!ndviAll$year %in% c(2005, 2012, 2023, 2024), ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
   # stat_smooth(data=ndviAll[ndviAll$yday<=max(ndviAll$yday[ndviAll$year==2023]) & ndviAll$year!=2023, ], aes(color="historical", fill="historical"), size=1.5, alpha=0.8) +
   stat_smooth(data=ndviAll[ndviAll$year==2005, ], aes(color="2005", fill="2005"), size=1, alpha=0.2, method="gam") +
   stat_smooth(data=ndviAll[ndviAll$year==2012, ], aes(color="2012", fill="2012"), size=1, alpha=0.2, method="gam") +
   stat_smooth(data=ndviAll[ndviAll$year==2023, ], aes(color="2023", fill="2023"), size=1.25, alpha=0.5, method="gam") +
+  stat_smooth(data=ndviAll[ndviAll$year==2024, ], aes(color="2024", fill="2024"), size=1.25, alpha=0.5, method="gam") +
   scale_x_continuous(name="Day of Year", expand=c(0,0), breaks=day.labels$yday[seq(2, 12, by=3)], labels=day.labels$Text[seq(2, 12, by=3)])  +
-  scale_color_manual(values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
-  scale_fill_manual(values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
+  scale_color_manual(values=c('2024'="dodgerblue2", '2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
+  scale_fill_manual(values=c('2024'="dodgerblue2", '2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
   coord_cartesian(ylim=c(0,1)) +
   labs(x="Day of Year")  +
   guides(fill=F) +
@@ -89,19 +103,20 @@ ggplot(data=ndviAll[ndviAll$type!="grassland",], aes(x=yday, y=NDVI)) +
   geom_line(data=ndviAll[ndviAll$year==2005 & ndviAll$type!="grassland", ], aes(color="2005"), size=0.25) +
   geom_line(data=ndviAll[ndviAll$year==2012 & ndviAll$type!="grassland", ], aes(color="2012"), size=0.25) +
   geom_line(data=ndviAll[ndviAll$year==2023 & ndviAll$type!="grassland", ], aes(color="2023"), size=0.5) +
-  stat_smooth(data=ndviAll[!ndviAll$year %in% c(2005, 2012, 2023) & ndviAll$type!="grassland", ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
+  geom_line(data=ndviAll[ndviAll$year==2024 & ndviAll$type!="grassland", ], aes(color="2024"), size=0.5) +
+  stat_smooth(data=ndviAll[!ndviAll$year %in% c(2005, 2012, 2023, 2024) & ndviAll$type!="grassland", ],  aes(color="historical", fill="historical"), size=1.5, alpha=0.5, method="gam") +
   # stat_smooth(data=ndviAll[ndviAll$yday<=max(ndviAll$yday[ndviAll$year==2023]) & ndviAll$year!=2023, ], aes(color="historical", fill="historical"), size=1.5, alpha=0.8) +
   stat_smooth(data=ndviAll[ndviAll$year==2005 & ndviAll$type!="grassland", ], aes(color="2005", fill="2005"), size=1, alpha=0.2, method="gam") +
   stat_smooth(data=ndviAll[ndviAll$year==2012 & ndviAll$type!="grassland", ], aes(color="2012", fill="2012"), size=1, alpha=0.2, method="gam") +
   stat_smooth(data=ndviAll[ndviAll$year==2023 & ndviAll$type!="grassland", ], aes(color="2023", fill="2023"), size=1.25, alpha=0.5, method="gam") +
+  stat_smooth(data=ndviAll[ndviAll$year==2024 & ndviAll$type!="grassland", ], aes(color="2024", fill="2024"), size=1.25, alpha=0.5, method="gam") +
   scale_x_continuous(name="Day of Year", expand=c(0,0), breaks=day.labels$yday[seq(2, 12, by=3)], labels=day.labels$Text[seq(2, 12, by=3)])  +
-  scale_color_manual(name="year", values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
-  scale_fill_manual(name = "year", values=c('2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
+  scale_color_manual(values=c('2024'="dodgerblue2", '2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
+  scale_fill_manual(values=c('2024'="dodgerblue2", '2023'="firebrick2", '2012'="goldenrod1", '2005'="darkorange2", 'historical'='black')) +
   coord_cartesian(ylim=c(0,1)) +
   labs(x="Day of Year")  +
   guides(fill=F) +
   theme_bw() + 
   theme(strip.text = element_text(size=rel(1.25), face="bold"))
-
 dev.off()
 
