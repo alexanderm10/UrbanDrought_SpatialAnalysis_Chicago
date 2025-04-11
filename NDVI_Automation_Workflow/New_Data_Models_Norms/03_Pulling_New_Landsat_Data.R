ea55885c0d174c916a35c2f555e9604fe997e766
Christy Rollinson <crollinson@mortonarb.org>
2025-04-11 13:53:02 -0500

    soft code filepath

--- NDVI_Automation_Workflow/New_Data_Models_Norms/03_Pulling_New_Landsat_Data.R
+++ NDVI_Automation_Workflow/New_Data_Models_Norms/03_Pulling_New_Landsat_Data.R
@@ -18,7 +18,8 @@ NDVIsave <- ("My Drive/UrbanEcoDrought_NDVI_LocalExtract")
 ##################### 
 # 0. Read in helper functions ----
 ##################### 
-source("/Users/jocelyngarcia/Documents/GitHub/UrbanDrought_SpatialAnalysis_Chicago/NDVI_Automation_Workflow/Baseline_Data_Models_Norms/00_EarthEngine_HelperFunctions copy.R")
+
+source("../Baseline_Data_Models_Norms/00_EarthEngine_HelperFunctions copy.R")
 
 
 
@@ -26,7 +27,7 @@ source("/Users/jocelyngarcia/Documents/GitHub/UrbanDrought_SpatialAnalysis_Chica
 #Reading in previous data to get latest date, will need later
 
 #Files generated from Juliana's workflow - will need these to autoupdate
-GAM_fit_NDVI <-read_csv("/Users/jocelyngarcia/Library/CloudStorage/GoogleDrive-jgarcia@mortonarb.org/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/raw_data_k=12.csv")%>%
+GAM_fit_NDVI <-read_csv(file.path(path.google.share, "data/NDVI_drought_monitoring/raw_data_k=12.csv"))%>%
   mutate(date = as.Date(date, format="%Y-%m-%d"))
 
 #putting NDVI_data in order by date
@@ -87,184 +88,6 @@ ee_print(Chicago)
 chiBounds <- Chicago$geometry()$bounds()
 chiBBox <- ee$Geometry$BBox(-88.70738, 41.20155, -87.52453, 42.49575)
 
-# https://developers.google.com/earth-engine/datasets/catalog/USGS_NLCD_RELEASES_2019_REL_NLCD
-nlcdChi <- ee$ImageCollection('USGS/NLCD_RELEASES/2019_REL/NLCD')$select('landcover')$map(function(img){
-  d <- ee$Date(ee$Number(img$get('system:time_start')));
-  y <- ee$Number(d$get('year'));
-  return(img$clip(Chicago)$set('year', y))
-})
-
-# https://developers.google.com/earth-engine/datasets/catalog/USGS_NLCD_RELEASES_2021_REL_NLCD#description
-nlcdChi21 <- ee$ImageCollection('USGS/NLCD_RELEASES/2021_REL/NLCD')$select('landcover')$map(function(img){
-  d <- ee$Date(ee$Number(img$get('system:time_start')));
-  y <- ee$Number(d$get('year'));
-  return(img$clip(Chicago)$set('year', y))
-})
-
-lcVals = nlcdChi$first()$reduceRegion(ee$Reducer$frequencyHistogram(), Chicago, maxPixels=1e12)
-lcVals$getInfo()
-
-# ee_print(nlcdChi) # Note: the nlcd is giving me a strsplit code error, but it does map!
-Map$addLayer(nlcdChi$first()$select('landcover'), nlcdvis, 'NLCD Land Cover',);
-Map$addLayer(nlcdChi21$first()$select('landcover'), nlcdvis, 'NLCD Land Cover',);
-
-
-projNLCD = nlcdChi$select("landcover")$first()$projection()
-projNLCD$getInfo() # Note that this is really messy
-projCRS = "EPSG:4326" # This seems to be what works
-projTransform <- unlist(projNLCD$getInfo()$transform) # I saved this, but using it in the export causes really weird things
-
-nlcdProj = "EPSG:4326"
-nlcdTransform = c(30, 0, -2493045, 0, -30, 3310005)
-
-##################### 
-# Create annually-resolved image ----
-##################### 
-lcChi2001 <- nlcdChi$filter(ee$Filter$eq('system:index', '2001'))$first();
-lcChi2004 <- nlcdChi$filter(ee$Filter$eq('system:index', '2004'))$first();
-lcChi2006 <- nlcdChi$filter(ee$Filter$eq('system:index', '2006'))$first();
-lcChi2008 <- nlcdChi$filter(ee$Filter$eq('system:index', '2008'))$first();
-lcChi2011 <- nlcdChi$filter(ee$Filter$eq('system:index', '2011'))$first();
-lcChi2013 <- nlcdChi$filter(ee$Filter$eq('system:index', '2013'))$first();
-lcChi2016 <- nlcdChi$filter(ee$Filter$eq('system:index', '2016'))$first();
-lcChi2019 <- nlcdChi$filter(ee$Filter$eq('system:index', '2019'))$first();
-lcChi2021 <- nlcdChi21$filter(ee$Filter$eq('system:index', '2021'))$first();
-
-# Creating duped layers for each year for our sanity
-lcChi2000 <- lcChi2001$set('system:time_start', ee$Date$fromYMD(2000, 1, 1))$set('year',2000);
-# lcChi2001 <- nlcdChi$filter(ee$Filter$eq('system:index', '2001'))$first();
-lcChi2002 <- lcChi2001$set('system:time_start', ee$Date$fromYMD(2002, 1, 1))$set('year',2002);
-lcChi2003 <- lcChi2004$set('system:time_start', ee$Date$fromYMD(2003, 1, 1))$set('year',2003);
-# lcChi2004 <- nlcdChi$filter(ee$Filter$eq('system:index', '2004'))$first();
-lcChi2005 <- lcChi2006$set('system:time_start', ee$Date$fromYMD(2005, 1, 1))$set('year',2005);
-# lcChi2006 <- nlcdChi$filter(ee$Filter$eq('system:index', '2006'))$first();
-lcChi2007 <- lcChi2008$set('system:time_start', ee$Date$fromYMD(2007, 1, 1))$set('year',2007);
-# lcChi2008 <- nlcdChi$filter(ee$Filter$eq('system:index', '2008'))$first();
-lcChi2009 <- lcChi2008$set('system:time_start', ee$Date$fromYMD(2009, 1, 1))$set('year',2009);
-lcChi2010 <- lcChi2011$set('system:time_start', ee$Date$fromYMD(2010, 1, 1))$set('year',2010);
-# lcChi2011 <- nlcdChi$filter(ee$Filter$eq('system:index', '2011'))$first();
-lcChi2012 <- lcChi2013$set('system:time_start', ee$Date$fromYMD(2012, 1, 1))$set('year',2012);
-# lcChi2013 <- nlcdChi$filter(ee$Filter$eq('system:index', '2013'))$first();
-lcChi2014 <- lcChi2013$set('system:time_start', ee$Date$fromYMD(2014, 1, 1))$set('year',2014);
-lcChi2015 <- lcChi2016$set('system:time_start', ee$Date$fromYMD(2015, 1, 1))$set('year',2015);
-# lcChi2016 <- nlcdChi$filter(ee$Filter$eq('system:index', '2016'))$first();
-lcChi2017 <- lcChi2016$set('system:time_start', ee$Date$fromYMD(2017, 1, 1))$set('year',2017);
-lcChi2018 <- lcChi2019$set('system:time_start', ee$Date$fromYMD(2018, 1, 1))$set('year',2018);
-# lcChi2019 <- nlcdChi$filter(ee$Filter$eq('system:index', '2019'))$first();
-lcChi2020 <- lcChi2019$set('system:time_start', ee$Date$fromYMD(2020, 1, 1))$set('year',2020);
-lcChi2021 <- lcChi2021$set('system:time_start', ee$Date$fromYMD(2021, 1, 1))$set('year',2021);
-lcChi2022 <- lcChi2021$set('system:time_start', ee$Date$fromYMD(2022, 1, 1))$set('year',2022);
-lcChi2023 <- lcChi2021$set('system:time_start', ee$Date$fromYMD(2023, 1, 1))$set('year',2023);
-lcChi2024 <- lcChi2021$set('system:time_start', ee$Date$fromYMD(2024, 1, 1))$set('year',2024);
-lcChi2025 <- lcChi2021$set('system:time_start', ee$Date$fromYMD(2025, 1, 1))$set('year',2025);
-
-collAnn <- ee$ImageCollection(c(lcChi2001, lcChi2002, lcChi2003, lcChi2004, lcChi2005, lcChi2006, lcChi2007, lcChi2008, lcChi2009, lcChi2010, lcChi2011, lcChi2012, lcChi2013, lcChi2014, lcChi2015, lcChi2016, lcChi2017, lcChi2018, lcChi2019, lcChi2020, lcChi2021, lcChi2022, lcChi2023, lcChi2024,lcChi2025))
-
-# Saving will be much easier if it's a single year with multiple bands
-yrLC <- ee$List(collAnn$aggregate_array("year"))$distinct()
-# yrLC$getInfo()
-yrString <- ee$List(paste0("YR", yrLC$getInfo()))
-
-lcChiAnn <- ee$ImageCollection$toBands(collAnn)$rename(yrString)
-# ee_print(lcChiAnn)
-Map$addLayer(lcChiAnn$select("YR2012"), nlcdvis, 'NLCD Land Cover');
-
-saveLandCover <- ee_image_to_asset(lcChiAnn, description="Save_NLCD-Chicago_AnnualDupe_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_AnnualDupe_2000-2024"), maxPixels = 10e9, scale=30, region = chiBounds, crs=projCRS, overwrite=T)
-saveLandCover$start()
-
-##################### 
-# # Define the land cover classes and their corresponding values
-
-# # Forest: 41,42,43 ----
-classFor = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(41)$Or(image$select('landcover')$eq(42))$Or(image$select('landcover')$eq(43));
-  return(image$updateMask(lcMask)$set('class', 'Forest')$set('year', d$get('year')));
-});
-
-forMask <- ee$ImageCollection$toBands(classFor)$rename(yrString)
-# ee_print(forMask)
-# Map$addLayer(forMask$select("YR2012"));
-
-saveForMask <- ee_image_to_asset(forMask, description="Save_lcMask-Forest_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Forest"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveForMask$start()
-# nlcdProj
-
-# # Grassland/Savanna/Grass: 51,52,71,72 ----
-classGrass = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(51)$Or(image$select('landcover')$eq(52))$Or(image$select('landcover')$eq(71))$Or(image$select('landcover')$eq(72));
-  return(image$updateMask(lcMask)$set('class', 'Grassland')$set('year', d$get('year')));
-});
-
-grassMask <- ee$ImageCollection$toBands(classGrass)$rename(yrString)
-
-saveGrassMask <- ee_image_to_asset(grassMask, description="Save_lcMask-Grass_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Grass"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveGrassMask$start()
-
-# # Crop = 81,82 ----
-classCrop = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(81)$Or(image$select('landcover')$eq(82));
-  return(image$updateMask(lcMask)$set('class', 'Crop')$set('year', d$get('year')));
-});
-
-cropMask <- ee$ImageCollection$toBands(classCrop)$rename(yrString)
-
-saveCropMask <- ee_image_to_asset(cropMask, description="Save_lcMask-Crop_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Crop"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveCropMask$start()
-
-
-# # Open Urban: 21 ----
-classUrbO = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(21);
-  return(image$updateMask(lcMask)$set('class', 'Urban-Open')$set('year', d$get('year')));
-});
-
-urbOMask <- ee$ImageCollection$toBands(classUrbO)$rename(yrString)
-
-saveUrbOMask <- ee_image_to_asset(urbOMask, description="Save_lcMask-Urban-Open_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Urban-Open"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveUrbOMask$start()
-
-
-# # Low Intensity Urban: 22 ----
-classUrbL = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(22);
-  return(image$updateMask(lcMask)$set('class', 'Urban-Low')$set('year', d$get('year')));
-});
-
-urbLMask <- ee$ImageCollection$toBands(classUrbL)$rename(yrString)
-
-saveUrbLMask <- ee_image_to_asset(urbLMask, description="Save_lcMask-Urban-Low_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Urban-Low"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveUrbLMask$start()
-
-
-# # Medium Intensity Urban: 23 ----
-classUrbM = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(23);
-  return(image$updateMask(lcMask)$set('class', 'Urban-Medium')$set('year', d$get('year')));
-});
-
-urbMMask <- ee$ImageCollection$toBands(classUrbM)$rename(yrString)
-
-saveUrbMMask <- ee_image_to_asset(urbMMask, description="Save_lcMask-Urban-Medium_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Urban-Medium"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveUrbMMask$start()
-
-
-# # High Intenstity Urban: 24 ----
-classUrbH = collAnn$map(function(image) {
-  d = ee$Date(ee$Number(image$get('system:time_start')))
-  lcMask = image$select('landcover')$eq(24);
-  return(image$updateMask(lcMask)$set('class', 'Urban-Low')$set('year', d$get('year')));
-});
-
-urbHMask <- ee$ImageCollection$toBands(classUrbH)$rename(yrString)
-
-saveUrbHMask <- ee_image_to_asset(urbHMask, description="Save_lcMask-Urban-High_2000-2024", assetId=file.path(assetHome, "NLCD-Chicago_2000-2024_Urban-High"), maxPixels = 10e9, scale=30, region = chiBounds, crs="EPSG:4326", overwrite=T)
-saveUrbHMask$start()
 #End of Christy's first script
 ####################################################################################################################
 # Setting the center point for the Arb because I think we'll have more variation
